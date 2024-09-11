7fb3368f0a4904845ebaac1f73b47ed31518f389
tqi6 <tqi6>
2024-09-11 20:55:05 +0000

    Added a table that outlines the meanings of flags used in pytest.main

--- tsdb/tests/nb_invoke_tests.py
+++ tsdb/tests/nb_invoke_tests.py
@@ -8,6 +8,19 @@ import sys
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC Pytest call meanings
+# MAGIC
+# MAGIC | flag | definition |
+# MAGIC |------|------------|
+# MAGIC | "." | Run all tests in cwd, this translates to tsdb/tests |
+# MAGIC | -v | Verbose |
+# MAGIC | -p no:cacheprovider | -p specifies plugins, this full command prevents pytest from caching results (since we can't write) |
+# MAGIC
+# MAGIC This is a basic pytest call and you can have more complex setups by passing different flags.
+
+# COMMAND ----------
+
 # May not have write permissions on databricks, so prevent __pycache__ creation
 sys.dont_write_bytecode = True
 retcode = pytest.main([".", "-v", "-p", "no:cacheprovider"])
