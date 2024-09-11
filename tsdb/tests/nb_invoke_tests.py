c775d43079acb021eee54bf3f0d513300278ae76
tqi6 <tqi6>
2024-09-11 17:01:13 +0000

    Updated test module docstring with more information, added comment on byte code

--- tsdb/tests/nb_invoke_tests.py
+++ tsdb/tests/nb_invoke_tests.py
@@ -8,6 +8,7 @@ import sys
 
 # COMMAND ----------
 
+# May not have write permissions on databricks, so prevent __pycache__ creation
 sys.dont_write_bytecode = True
 retcode = pytest.main([".", "-v", "-p", "no:cacheprovider"])
 
--- tsdb/tests/test_functions_transformations.py
+++ tsdb/tests/test_functions_transformations.py
@@ -1,5 +1,6 @@
 """
-This module tests code in tsdb.preprocessing.functions that are transformation based, aka
+This module tests code in tsdb.preprocessing.functions that are transformation based. If needed,
+function docstrings can include examples of what is being tested.
 """
 import pytest
 import tsdb.preprocessing.functions as tsf
@@ -9,7 +10,10 @@ import pyspark.sql.functions as F
 
 
 @pytest.fixture(scope="module")
-def spark():
+def spark() -> SparkSession:
+    """
+    Returns the SparkSession to be used in tests that require Spark
+    """
     spark = (
         SparkSession.builder.master("local")
         .appName("test_functions_transformation")
@@ -19,15 +23,16 @@ def spark():
     return spark
 
 
-def test_cast_to_column_string_input(spark):
+def test_cast_to_column_string_input(spark: SparkSession) -> None:
     """
-    What are you testing?
-    Why are you testing
+    Tests the cast_to_column function with a string input and verifies it returns
+    a column object. Column objects are oftenmore useful than strings but we still
+    want the flexibility of being able to pass a column name as an input.
     """
     col_name = "test_column"
     test_dataframe = spark.createDataFrame([(1,), (2,), (3,)], [col_name])
     
     result = tsf.cast_to_column(col_name)
 
-    # This should be a PySpark Column object
-    assert isinstance(result, F.Column), f"Expected a Column object, got {type(result)}"
+    # Result should be a PySpark Column object
+    assert isinstance(result, F.Column), f"Expected a PySpark Column object, got {type(result)}"
