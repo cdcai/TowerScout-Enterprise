7d07f53d58fb168afe1bbbf17d5322c26206dfe8
tqi6 <tqi6>
2024-09-13 16:36:39 +0000

    Deleted packaging files to declutter repo. Will extend .gitignore in future commit.

--- tsdb/development/build/lib/tower_scout_library/utils.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def calculate_square_root(number):
-    """
-    This function calculates the square root of a given number.
-    """
-    if number < 0:
-        raise ValueError("Cannot calculate the square root of a negative number.")
-    return math.sqrt(number)
