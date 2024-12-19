a79056f48b9cdd3013f9eb4a5fa514e60c64cacf
ztm8 <ztm8>
2024-12-19 15:00:45 +0000

    adding functions to utilize mosaic streaming

---
+++ tsdb/development/mosaic-testing.py
@@ -0,0 +1,231 @@
+# Databricks notebook source
+# MAGIC %pip install mosaicml-streaming
+
+# COMMAND ----------
+
+from functools import partial
+
+import numpy as np
+from PIL import Image
+from shutil import rmtree
+from uuid import uuid4
+from streaming import MDSWriter  # mosiacml-streaming
+import pyspark.sql.functions as F
+from pyspark.sql.column import Column
+from pyspark.sql import DataFrame
+
+import torchvision
+import torch
+from collections import defaultdict
+
+from torch.utils.data import DataLoader
+from streaming import StreamingDataset  # mosiacml-streaming
+
+from ultralytics.cfg import get_cfg
+from ultralytics.nn.tasks import attempt_load_one_weight, DetectionModel
+from tsdb.ml.utils import OptimizerArgs
+from tsdb.ml.yolo_trainer import YoloModelTrainer
+from tsdb.ml.utils import OptimizerArgs
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Make these into funcitons
+
+# COMMAND ----------
+
+def get_proper_path(path: "ColumnOrName") -> Column:
+    # remove the dbfs: prefix as that causes errors when reading
+    file_with_extension = F.element_at(F.split(path, ":"), 2)
+    return file_with_extension
+
+df = (
+    spark
+    .read
+    .format("delta")
+    .table("edav_dev_csels.towerscout.test_image_silver")
+    .selectExpr("image_path as im_file", "bboxes")
+    .where("processing_time > '2024-12-18'")
+    .limit(1000)
+)
+
+
+df = df.withColumn("im_file", get_proper_path("im_file"))
+df = df.filter(F.size(F.col("bboxes")) > 0)  # filter out rows with no bboxes detected
+df = df.withColumn("cls", F.expr("transform(bboxes, x -> float(0))") )  # for each bbox create a label `0`
+df = df.withColumn("bboxes", F.expr("transform(bboxes, x -> array(x.x1, x.y1, x.x2, x.y2))") )  # only keep box coord keys from bboxes struct
+df = df.withColumn("bboxes", F.flatten(F.col("bboxes")) )
+display(df)
+
+# COMMAND ----------
+
+def convert_to_mds(
+    df: DataFrame, columns: dict[str, str], compression: str, out_root: str, **kwargs
+) -> None:
+    """
+    Function that converts a Spark DataFrame to a collection of `.mds` files.
+
+    Args:
+    df: The spark dataframe to be converted
+    columns: A dictionary which contains the column names of the dataframe
+            as keys and the corresponding mds data types as values
+    compression: Compression algorithm name to use
+    out_root: The local or remote directory path to store the output compressed files
+    """
+    pd_df = df.toPandas()
+    samples = pd_df.to_dict("records")
+
+    for sample in samples:
+        sample["img"] = Image.open(sample["im_file"])
+        sample["ori_shape"] = np.array(sample["img"].size, dtype=np.uint32)
+
+    # Use `MDSWriter` to iterate through the input data and write to a collection of `.mds` files.
+    with MDSWriter(
+        out=out_root, columns=columns, compression=compression, **kwargs
+    ) as out:
+        for sample in samples:
+            out.write(sample)
+
+
+def collate_fn_img(data, transforms: callable):
+    result = defaultdict(list)
+
+    for i, element in enumerate(data):
+        for key, value in element.items():
+            if key == "img":
+                value = transforms(value)
+
+                _, h, w = value.shape  # h & w after sime augmentation has been don
+
+                result["resized_shape"].append((h, w))
+                result["ratio_pad"].append(
+                    (
+                        result["resized_shape"][i][0] / element["ori_shape"][0],
+                        result["resized_shape"][i][1] / element["ori_shape"][1],
+                    )
+                )  # from https://github.com/ultralytics/ultralytics/blob/main/ultralytics/data/base.py#L295
+
+            result[key].append(value)
+
+        num_boxes = len(
+            element["cls"]
+        )  # no need to use reshape b/c len of cls array tells us how many bboxes
+
+        for _ in range(num_boxes):
+            result["batch_idx"].append(
+                float(i)
+            )  # yolo dataloader has this as a float not int
+
+    result["img"] = torch.stack(result["img"], dim=0)
+    result["batch_idx"] = torch.tensor(result["batch_idx"])
+
+    # Unit test reshaping
+    result["bboxes"] = torch.tensor(np.concatenate(result["bboxes"]).reshape(-1, 4))
+    result["cls"] = torch.tensor(
+        np.concatenate(result["cls"], dtype=float).reshape(-1, 1)
+    )
+
+    result["im_file"] = tuple(result["im_file"])
+    result["ori_shape"] = tuple(result["ori_shape"])
+    result["resized_shape"] = tuple(result["resized_shape"])
+    result["ratio_pad"] = tuple(result["ratio_pad"])
+
+    return dict(result)
+
+
+def get_dataloader(
+    local_dir: str, remote_dir: str, batch_size: int, **kwargs
+) -> DataLoader:
+    """
+    Function that creates a PyTorch DataLoader from a collection of `.mds` files.
+
+    Args:
+    local_dir: Local directory where dataset is cached during training
+    remote_dir: The local or remote directory where dataset .mds files are stored
+    batch_size: The batch size of the dataloader and dataset.
+          See: https://docs.mosaicml.com/projects/streaming/en/stable/getting_started/faqs_and_tips.html
+    """
+    dataset = StreamingDataset(
+        local=local_dir,
+        remote=remote_dir,
+        batch_size=batch_size,
+        split=None,
+        shuffle=True,
+        **kwargs
+    )
+
+    transforms = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
+        ]
+    )
+
+    # Create PyTorch DataLoader
+    collate_fn = partial(collate_fn_img, transforms=transforms)
+    dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn)
+    return dataloader
+
+# COMMAND ----------
+
+!rm -rf /tmp/ztm8/mosaicml/
+
+# COMMAND ----------
+
+out_root = '/tmp/ztm8/mosaicml/'
+
+
+columns = {
+    'im_file': 'str',
+    'img': 'jpeg',
+    'bboxes': 'ndarray:float32',
+    'cls': 'ndarray:float32',
+    'ori_shape': 'ndarray:uint32'
+}
+
+compression = 'zstd'
+
+convert_to_mds(df, columns, compression, out_root)
+
+# COMMAND ----------
+
+!rm -rf /local/cache/path6
+
+# COMMAND ----------
+
+remote_dir = out_root
+
+local_dir = '/local/cache/path6'
+
+batch_size_dataset, batch_size_dataloader = 1, 64
+
+dataloader = get_dataloader(local_dir, remote_dir, batch_size_dataset, batch_size_dataloader)
+
+# COMMAND ----------
+
+for i, batch in enumerate(dataloader):
+    print(batch)
+    break
+
+# COMMAND ----------
+
+model_yaml = "yolov10n.yaml"
+model_pt = "yolov10n.pt"
+
+args = get_cfg()  # used to get hyperparams for model and other stuff from some config file
+model = DetectionModel(cfg=model_yaml, verbose=False)
+weights, _ = attempt_load_one_weight(model_pt)
+model.load(weights)
+model.nc = 1
+model.names ='ct'
+model.args = args
+model.args.single_cls = True # set to False when using coco dataset since it has 80 classes
+
+
+optimizer_args = OptimizerArgs(optimizer_name="Adam", lr0=0.001, momentum=0.99)
+yolo_trainer = YoloModelTrainer(optimizer_args=optimizer_args, model=model)
+
+# COMMAND ----------
+
+for i, batch in enumerate(dataloader):
+    metrics = yolo_trainer.training_step(batch)
+    print(f"Training metrics: {metrics}")
--- tsdb/development/yolo_tuner.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-from ultralytics import YOLO
-
-model = YOLO("yolo11n.pt")
-results = model.tune(use_ray=True, iterations=3)
-print(results)
